55fbd1983a939dbb76883aaf9ecde5e07965a490
jerrychoux <jerrychoux@163.com>
2021-02-17 22:25:46 +0800

    添加生成敌人

--- scripts/demo/main.tsx
+++ scripts/demo/main.tsx
@@ -2,6 +2,7 @@ import { tool, property, node } from "../decorators";
 import Player from "./player";
 
 import ENEMY from "res://scenes/demo/enemy.tscn";
+import Enemy from "./enemy";
 
 @tool
 export default class Main extends godot.Node2D {
@@ -15,14 +16,36 @@ export default class Main extends godot.Node2D {
 
   _on_startTimer_timeout() {
     let enemyTimer = this.$("enemyTimer") as godot.Timer;
-    enemyTimer.stop();
+    enemyTimer.start();
+    let scoreTimer = this.$("scoreTimer") as godot.Timer;
+    scoreTimer.start();
   }
 
   _on_scoreTimer_timeout() {
     this.score += 1;
   }
 
-  _on_enemyTimer_timeout() {}
+  _on_enemyTimer_timeout() {
+    let pathFollow = this.$("enemyPath/enemyPathFollow") as godot.PathFollow2D;
+    pathFollow.offset = godot.randi();
+
+    let direction =
+      pathFollow.rotation +
+      godot.PI / 2 +
+      godot.rand_range(-godot.PI / 4, godot.PI / 4);
+
+    let enemy = (ENEMY as godot.PackedScene).instance() as godot.RigidBody2D;
+    enemy.position = pathFollow.position;
+    enemy.rotation = direction;
+
+    let enemyScript = enemy as Enemy;
+    enemy.linear_velocity = new godot.Vector2(
+      godot.rand_range(enemyScript.minSpeed, enemyScript.maxSpeed),
+      0
+    ).rotated(direction);
+
+    this.add_child(enemy);
+  }
 
   gameOver() {
     let scoreTimer = this.$("scoreTimer") as godot.Timer;
@@ -31,8 +54,6 @@ export default class Main extends godot.Node2D {
 
   gameNew() {
     this.score = 0;
-    let enemy = (ENEMY as godot.PackedScene).instance();
-    this.add_child(enemy);
 
     let startPosition = this.$("startPosition") as godot.Position2D;
     let player = this.$("player") as Player;
